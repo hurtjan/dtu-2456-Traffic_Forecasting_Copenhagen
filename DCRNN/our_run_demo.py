56de7dc7cd1405a4c7558c4978bc7d29347cf5fe
s182071 <hurt0jan@JansPC.localdomain>
2019-01-03 19:05:05 +0100

    initial

---
+++ DCRNN/our_run_demo.py
@@ -0,0 +1,37 @@
+import argparse
+import numpy as np
+import os
+import sys
+import tensorflow as tf
+import yaml
+
+from lib.utils import load_graph_data
+from model.dcrnn_supervisor import DCRNNSupervisor
+
+
+def run_dcrnn(args):
+    graph_pkl_filename = 'data/sensor_graph/our_adj_mx.pkl'
+    with open(args.config_filename) as f:
+        config = yaml.load(f)
+    tf_config = tf.ConfigProto()
+    if args.use_cpu_only:
+        tf_config = tf.ConfigProto(device_count={'GPU': 0})
+    tf_config.gpu_options.allow_growth = True
+    _, _, adj_mx = load_graph_data(graph_pkl_filename)
+    with tf.Session(config=tf_config) as sess:
+        supervisor = DCRNNSupervisor(adj_mx=adj_mx, **config)
+        supervisor.load(sess, config['train']['model_filename'])
+        outputs = supervisor.evaluate(sess)
+        np.savez_compressed(args.output_filename, **outputs)
+        print('Predictions saved as {}.'.format(args.output_filename))
+
+
+if __name__ == '__main__':
+    sys.path.append(os.getcwd())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--use_cpu_only', default=False, type=str, help='Whether to run tensorflow on cpu.')
+    parser.add_argument('--config_filename', default='data/our_model/dcrnn_DR_2_h_12_64-64_lr_0.01_bs_64_1227185402/config_81.yaml', type=str,
+                        help='Config file for pretrained model.')
+    parser.add_argument('--output_filename', default='data/our_dcrnn_predictions.npz')
+    args = parser.parse_args()
+    run_dcrnn(args)
